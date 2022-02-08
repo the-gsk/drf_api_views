fae9723426e037dce3284a1714e0c1ea6bdfb7f3
Gaurav Shankar Kumar <gauravshankarkumar@gmail.com>
2022-02-08 22:44:53 +0530

    Model View Set

---
+++ 6 Model View Set (CRUD Operations) /backend/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api import views
+
+# creating router object
+router = DefaultRouter()
+
+# register StudentModelViewsets with router
+router.register('studentapi', views.StudentModelViewset, basename='student')
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
